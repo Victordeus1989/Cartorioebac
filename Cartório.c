922f56303be39e8fa24826d028c763c3be29af29
Victordeus1989 <victordeus1989@gmail.com>
2025-05-01 23:01:05 +0100

    opção de saida
    inclusão a opção de saída para o usuário

--- Cartório.c
+++ Cartório.c
@@ -131,7 +131,8 @@ int main()
 			printf("Escolha a opção desejada do menu:\n\n");
 			printf("\t1 - Registrar nomes\n");
 			printf("\t2 - Consultar nomes\n");
-			printf("\t3 - Apagar nomes\n\n"); 
+			printf("\t3 - Apagar nomes\n");
+			printf("\t4 - Voltar ao inicio\n"); 
 			printf("Opção: "); // Fim do menu
 	
 			scanf("%d", &opcao); // Armazenando as informações do usuario
@@ -152,6 +153,12 @@ int main()
 			case 3:
 			apagar();
 			break;
+			
+			case 4:
+			printf("Obrigado por usar nosso programa !\n");
+			return 0;
+			break;
+			
 			
 			default:
 			printf("Essa opção não está disponivel!\n");
